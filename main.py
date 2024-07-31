0b8245806dc88c4d28000ec6d2007eaa302162ae
Yulia Demchyna <yuliademchyna@gmail.com>
2024-07-31 13:22:44 +0200

    Retrieve necessary info for email_body

--- main.py
+++ main.py
@@ -15,7 +15,15 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 # This is your Stripe CLI webhook secret for testing your endpoint locally.
 endpoint_secret = os.getenv("STRIPE_ENDPOINT_SECRET")
 
+#about the company and product
+article_number = os.getenv('ARTIKEL_NR')
+item_name = os.getenv('VARE_NAVN')
+item_quantity = os.getenv('VARE_ANTALL')
+organization_number = os.getenv('ORGANISASJONSNUMMER')
+
 app = FastAPI()
+
+
 
 @app.post("/webhook_succesful_payment")
 async def webhook(request: Request):
@@ -38,8 +46,47 @@ async def webhook(request: Request):
     if event['type'] == 'checkout.session.async_payment_succeeded' or (event['type'] == 'checkout.session.completed' and event['data']['object']['payment_status'] == 'paid'):
         session = event['data']['object']
         print(session)
-        #TODO get customer details here
 
+        customer_details = event['data']['object']['customer_details']
+        address = customer_details['address']
+        email = customer_details['email']
+        phone = customer_details['phone']
+
+        # Formatting the address
+        formatted_address = f"{address['line1']}, {address['city']}, {address['state']}, {address['postal_code']}, {address['country']}"
+
+        # Print extracted information
+        # print("Delivery Address:", formatted_address)
+        # print("Email:", email)
+        # print("Phone:", phone if phone else "No phone number provided")
+
+        email_body = f"""
+            Hei,
+
+            Følgende info for å sende inn en innkjøpsordre:
+
+            Artikkel-nr: {article_number}
+            Vare Navn: {item_name}
+            Antall: {item_quantity}
+
+            Leveringsadresse:
+            {address['line1']}
+            {address['line2']}
+            {address['postal_code']} {address['city']}
+            {address['country']}
+
+            Organisasjonsnummer det skal faktureres til:
+            {organization_number}
+
+            Kundeinformasjon:
+            E-post: {email}
+            Telefon: {phone}
+
+            Med vennlig hilsen,
+            ANTIDIET
+            """
+
+        print(email_body)
         #TODO do sendgrid email sending
     else:
         print(f'Unhandled event type {event["type"]}')
